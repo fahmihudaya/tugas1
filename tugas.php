992f68ccd24cc596ab92efd4b37897ebeb6b55c3
fahmihudaya <55371316+fahmihudaya@users.noreply.github.com>
2019-09-21 20:54:48 +0700

    Add files via upload

---
+++ tugas.php
@@ -0,0 +1,42 @@
+<?php
+
+$x = 35;
+$y = 25;
+
+echo "Luas Persegi Panjang adalah P x L";
+echo "<br>";
+echo "Panjang = ". $x;
+echo "<br>";
+echo "Lebar = ". $y;
+echo "<br>";
+echo "Luas Persegi Panjang adalah = ". $x*$y;
+
+echo "<br>";
+echo "<br>";
+
+$phi = 3.14;
+$r = 12;
+
+echo "Luas Lingkaran adalah phi*r*r ";
+echo "<br>";
+echo "phi = ". $phi;
+echo "<br>";
+echo "Panjang Jari-jari = ". $r;
+echo "<br>";
+echo "Luas Lingkaran adalah = ". $phi * $r * $r;
+
+echo "<br>";
+echo "<br>";
+
+$x = 25;
+$y = 15;
+
+echo "Luas Jajar Genjang adalah a x t ";
+echo "<br>";
+echo "Alas = ". $x;
+echo "<br>";
+echo "Tinggi = ". $y;
+echo "<br>";
+echo "Luas Jajar Genjang adalah = ". $x*$y;
+
+?>
